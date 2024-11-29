d5ee962b69c1af11c1e4b1b072533b18cf1eb622
Godongwoo <rhehddnvv12@gmail.com>
2024-11-30 03:15:07 +0900

    Update module-info.java
    모듈 추가

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.banuh.frologue {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
+    
     opens com.banuh.frologue to javafx.fxml;
     exports com.banuh.frologue;
 }
