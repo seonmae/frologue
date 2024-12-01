c4691e99a5f7697f9e262ffee8ec1e8221e0eb2c
seonmae <165996696+seonmae@users.noreply.github.com>
2024-12-01 19:23:07 +0900

    StartPage

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module com.banuh.frologue {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.media;
 
     opens com.banuh.frologue to javafx.fxml;
+    opens com.banuh.frologue.game.scenes to javafx.graphics;
     exports com.banuh.frologue;
+    exports com.banuh.frologue.game.scenes;
 }
